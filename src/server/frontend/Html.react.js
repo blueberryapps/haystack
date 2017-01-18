25eedd413e2180efc0e57b42173012d922ad7d01
Jan Brecka <jan.brecka@gmail.com>
2017-01-18 15:28:43 +0100

    change Html component to stateless function component

--- src/server/frontend/Html.react.js
+++ src/server/frontend/Html.react.js
@@ -1,28 +1,23 @@
 import React, { Component, PropTypes as RPT } from 'react';
 
-export default class Html extends Component {
+const Html = ({ children }) => (
+  <html lang="en">
+    <head>
+      <meta charSet="utf-8" />
+      <meta
+        content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"
+        name="viewport"
+      />
+      <meta content="ie=edge" httpEquiv="x-ua-compatible" />
+    </head>
+    <body>
+      {children}
+    </body>
+  </html>
+);
 
-  static propTypes = {
-    children:  RPT.oneOfType([RPT.node, RPT.string, RPT.element]).isRequired
-  };
+Html.propTypes = {
+  children:  RPT.oneOfType([RPT.node, RPT.string, RPT.element]).isRequired
+};
 
-  render() {
-    const { children } = this.props;
-
-    return (
-      <html lang="en">
-        <head>
-          <meta charSet="utf-8" />
-          <meta
-            content="width=device-width, initial-scale=1, shrink-to-fit=no, user-scalable=no"
-            name="viewport"
-          />
-          <meta content="ie=edge" httpEquiv="x-ua-compatible" />
-        </head>
-        <body>
-          {children}
-        </body>
-      </html>
-    );
-  }
-}
+export default Html;
