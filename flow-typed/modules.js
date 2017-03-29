6b7ba0558328213d4c66784cd7b95c93a5819293
Albert Němec <albert.nemec2457@gmail.com>
2017-03-29 11:24:51 +0200

    add flow-type and configurate it

---
+++ flow-typed/modules.js
@@ -0,0 +1,8 @@
+/*
+  If Flow throws "Required module not found", altough module is not missing,
+  add new "declare module" statement here for that library.
+*/
+
+declare module 'react-helmet' {
+  declare module.exports: any;
+}
--- src/browser/App.react.js
+++ src/browser/App.react.js
@@ -1,3 +1,4 @@
+// @flow
 import Helmet from 'react-helmet';
 import React from 'react';
 import logo from '../../assets/images/haystack_logo.png';
@@ -5,7 +6,7 @@ import logo from '../../assets/images/haystack_logo.png';
 const App = () => (
   <div>
     <Helmet title="Haystack" />
-    <button onClick={e => window.console.log(e)}>
+    <button onClick={(e: Event) => window.console.log(e)}>
       Haystack
       <img alt="logo" src={logo} />
     </button>
