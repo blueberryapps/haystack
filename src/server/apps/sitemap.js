7f6b0436d0db4493b6512ba85f1e3304cd123a17
Ondrej Bartas <ondrej@bartas.cz>
2017-05-24 14:12:54 +0200

    Add sitemap and robots.txt

---
+++ src/server/apps/sitemap.js
@@ -0,0 +1,22 @@
+import express from 'express';
+
+const app = express();
+
+const pages = [
+  '/',
+];
+
+app.use('/sitemap.xml', (req, res) => {
+  res.send(`
+    <?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9">
+      ${pages.map(page => `<url><loc>${page}</loc></url>`).join('\n')}
+    </urlset>
+  `);
+});
+
+app.on('mount', () => {
+  console.log('Sitemap is available at %ssitemap.xml', app.mountpath);
+});
+
+export default app;
